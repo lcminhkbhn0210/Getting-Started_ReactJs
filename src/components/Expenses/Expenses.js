8a405d4a72bc49f4a11f2af06b0956ffb92fb2e6
lcminh <lcminhthpt2000@gmail.com>
2023-10-11 21:41:19 +0700

    update 11/10/2023 night

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,30 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
-  const [filterInfoText, setFilterInfoText] = useState('2019, 2021 & 2022');
-  const filterChangeHandler = selectedYear => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filteredYear === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filterInfoText === "2020") {
+    filterInfoText = "2019, 2021 & 2022";
+  } else if (filterInfoText === "2022") {
+    filterInfoText = "2019, 2020 & 2021";
+  } else {
+    filterInfoText = "2019, 2020 & 2022";
+  }
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    if(selectedYear === '2019') {
-      setFilterInfoText('2020, 2021 & 2022');
-    }else if(selectedYear === '2020'){
-      setFilterInfoText('2019, 2021 & 2022')
-    }else if(selectedYear === '2022'){
-      setFilterInfoText('2019, 2020 & 2021');
-    }else{
-      setFilterInfoText('2019, 2020 & 2022');
-    }
   };
 
   return (
     <div>
-      <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
         <p>Data for years{filterInfoText} is hidden</p>
         <ExpenseItem
           title={props.items[0].title}
